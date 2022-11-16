f6788d4b5683fe5061c4fc2ad6947f8fddaef676
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-11-16 12:52:09 -0500

    da

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ var form = document.getElementById('novoItem')
 
 form.addEventListener('submit' , (e) => {
     e.preventDefault(e)
+    let nome = e.target[0].value
+    let quant = e.target[1].value
 })
