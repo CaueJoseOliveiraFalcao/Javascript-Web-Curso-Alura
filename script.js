f875b8638e8cdfc770e2c2f0cf4f5765fa863706
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-12-20 12:34:36 -0500

    up

--- script.js
+++ script.js
@@ -1,53 +1,51 @@
-let form = document.querySelector(".adicionar")
-const itens = JSON.parse(localStorage.getItem('itens')) || []
-const lista = document.querySelector(".lista")
-
-
-
+const form = document.getElementById("novoItem") 
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
 itens.forEach(element => {
-    CriarElemento(element)
+    criaElemento(element)
 });
+form.addEventListener("submit", (evento) => {  
+    evento.preventDefault()
 
+    const nome = evento.target.elements['nome'].value
+    const quantidade = evento.target.elements['quantidade'].value
 
-form.addEventListener('submit' , (e) => {
-    e.preventDefault(e)
+    const itemAtual = {
+        'nome':nome,
+        'quantidade':quantidade
+    }
+    
+    const existe = itens.find(element => element.nome === nome)
+    if (existe){
+        itemAtual.id = existe.id
+        AtualizaItem(itemAtual)
+    }
+    else {
+        itemAtual.id = itens.length
+        console.log(itemAtual.id)
+        criaElemento(itemAtual)
 
-    let nome = e.target.elements['nome'].value.trim()
-    let quantidade = e.target.elements['quantidade'].value
-
-    if (nome.length > 0 && quantidade.length > 0 && quantidade > 0 ){
-        const existe = itens.find(elemento => elemento.nome === nome)
-        if (existe != ''){
-            existe.quantidade +=quantidade
-        }
-        const itemAtual = {
-            'nome':nome,
-            'quantidade':quantidade
-        }
-
-        CriarElemento(itemAtual)
         itens.push(itemAtual)
-        localStorage.setItem('itens',JSON.stringify(itens))
+    }
+    localStorage.setItem('itens',JSON.stringify(itens))
 
-        e.target.elements['nome'].value = ''
-        e.target.elements['quantidade'].value = ''
-        
-    }
-    else{
-        alert("Preencha os campos corretamente")
-    }
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function CriarElemento(item){
-   let novoItem = document.createElement('li')
-   novoItem.classList.add('item')
+function criaElemento(item) {  
+    const NovoItem = document.createElement('li')
+    NovoItem.classList.add('item')
+    NovoItem.innerHTML += item.nome
+    
+    const NovoNum = document.createElement("strong")
+    NovoNum.innerHTML += item.quantidade
+    NovoNum.dataset.id = item.id
 
-   let novoNum = document.createElement("strong")
-   novoNum.innerHTML += item.quantidade
-   novoItem.appendChild(novoNum)
-   novoItem.innerHTML += item.nome
-
-   lista.appendChild(novoItem)
-
+    NovoItem.appendChild(NovoNum)
+    lista.appendChild(NovoItem)
+}
+function AtualizaItem(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
